f324951ac5c5d6695999817129e2b434a1c3e99b
Andy Fiedler <andyfidd@gmail.com>
2019-08-19 16:32:43 -0500

    andy finished location route

---
+++ server.js
@@ -0,0 +1,31 @@
+const express =require('express');
+
+const app =express();
+
+//localhost:3000/location?location=Blah
+app.get('/location', (request, response) => {
+    try{
+        const geoData = require('./geo.json');
+        const location = new Location(request.query.location, geoData)
+        response.send(location);
+    } catch(error){
+        response.status(500).send("Sorry! Something went wrong.")
+    }
+    
+});
+
+function Location(query, geoData){
+    this.search_query = query,
+    this.formatted_query = geoData.results[0].formatted_address;
+    this.latitude = geoData.results[0].geometry.location.lat;
+    this.longitude = geoData.results[0].geometry.location.lng;
+}
+
+app.get('/weather', (request, response) => {
+    response.send('it works');
+});
+
+app.listen(3000, () => {
+    console.log('server is listening');
+});
+
