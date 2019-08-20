1887fc806aeeaf3120a8c23937eddd8277ea3655
scottbisese <bisese.scott@gmail.com>
2019-08-20 16:27:34 -0500

    did that thing we did in class

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # cityExplora
 class 6 broh
 feature 1 and 2 done monday august 19th during lecture demo
-\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-feature 3 and 4 took 2 hours. Andy AKA GarbageJuice AKA jeejay and I (scott) were able to figure it out with mucho help from insctructor David. Think we learned sum stuff dough.
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+feature 3 and 4 took 2 hours. Andy AKA GarbageJuice AKA jeejay and I (scott) were able to figure it out with mucho help from insctructor David. 
+Think we learned sum stuff dough.
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
--- server.js
+++ server.js
@@ -10,13 +10,14 @@ app.use(cors());
 //route for location
 app.get('/location', (request, response) => {
     try{
-        superagent.get(`https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY`)
-        const location = new Location(request.query.location, geoData)
+        superagent.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODEAPI_KEY}`)
+        .then((geoData) => {
+        const location = new Location(request.query.data, geoData.body);
         response.send(location);
+        });
     } catch(error){
-        response.status(500).send("Sorry! Something went wrong.")
+    response.status(500).send("Sorry! Something went wrong.")
     }
-    
 });
 
 //route for weather
