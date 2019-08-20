16ce5801cbe4cdb4ee35e7efe58c45280a72b44f
Andy Fiedler <andyfidd@gmail.com>
2019-08-20 11:54:48 -0500

    andy commented out weather route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app =express();
 app.use(cors());
 
 //localhost:3000/location?location=Blah
+
+
+
+//route for location
 app.get('/location', (request, response) => {
     try{
         const geoData = require('./geo.json');
@@ -17,19 +21,22 @@ app.get('/location', (request, response) => {
     
 });
 
+//route for weather
 app.get('/weather', (request, response) => {
     const darkData = require('./darkysky.json');
 
     let weatherResults = []
 
+//fills weatherResults array 
     darkData.daily.data.forEach(day => {
+//
         weatherResults.push(new Weather(day));
     });
-
+//return results
     response.send(weatherResults);
 });
 
-
+//location contstructor
 function Location(query, geoData){
     this.search_query = query,
     this.formatted_query = geoData.results[0].formatted_address;
@@ -37,9 +44,13 @@ function Location(query, geoData){
     this.longitude = geoData.results[0].geometry.location.lng;
     
 }
+//weather contruction
 function Weather(darkData){
+//unformatted day in seconds
     let rawTime = darkData.time;
     this.forecast = darkData.summary;
+//use built in Date constructor. This example is odd because it has time in seconds.
+//(0, 15): first and last characters
     this.time = new Date (rawTime * 1000).toString().slice(0,15);
 };
 
